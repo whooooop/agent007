4779856268d62079b4d517bb233af9d97adee03b
sergeysavin <serzz1990@gmail.com>
2025-01-03 00:33:07 +0400

    add auth init pipe

--- init.mjs
+++ init.mjs
@@ -1,18 +1,20 @@
 import input from 'input'
 import { Storage } from './utils/storage.mjs'
+import { auth } from './telegram.mjs'
 
 const storageTelegram = new Storage('telegram');
 
 export async function init () {
     const useTelegramClientApp = await input.confirm('Use Telegram client APP');
     if (useTelegramClientApp) {
-        console.log('Create your app here', 'https://my.telegram.org/auth')
+        console.log('Create your app here', 'https://my.telegram.org/auth');
         const config = await storageTelegram.read('config', {});
         await storageTelegram.write('config', {
             apiId: await input.text('Telegram API ID:', { default: config.apiId }),
             apiHash: await input.text('Telegram API Hash:', { default: config.apiHash }),
             logChatId: await input.text('Telegram log Chat id:', { default: config.logChatId }),
         });
+        await auth();
     }
 }
 
