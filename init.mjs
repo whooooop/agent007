6c6d52492ffb9c164c10a1414333cf27571a4240
sergeysavin <serzz1990@gmail.com>
2025-01-03 00:26:23 +0400

    add commands

--- init.mjs
+++ init.mjs
@@ -6,10 +6,12 @@ const storageTelegram = new Storage('telegram');
 export async function init () {
     const useTelegramClientApp = await input.confirm('Use Telegram client APP');
     if (useTelegramClientApp) {
+        console.log('Create your app here', 'https://my.telegram.org/auth')
+        const config = await storageTelegram.read('config', {});
         await storageTelegram.write('config', {
-            apiId: await input.text('Telegram API ID:'),
-            apiHash: await input.text('Telegram API Hash:'),
-            logChatId: await input.text('Telegram log Chat id:'),
+            apiId: await input.text('Telegram API ID:', { default: config.apiId }),
+            apiHash: await input.text('Telegram API Hash:', { default: config.apiHash }),
+            logChatId: await input.text('Telegram log Chat id:', { default: config.logChatId }),
         });
     }
 }
