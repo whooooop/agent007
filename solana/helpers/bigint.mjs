95fd364f5c4775dc907584ab086a0e39c96ede5e
sergeysavin <serzz1990@gmail.com>
2025-01-07 23:36:33 +0400

    format number

--- solana/helpers/bigint.mjs
+++ solana/helpers/bigint.mjs
@@ -1,6 +1,19 @@
 export const absBigInt = (value) => value < 0n ? -value : value;
 
-export function applyDecimalsBigInt(rawAmount, decimals) {
+export function applyDecimalsBigInt(rawAmount, decimals, format = true) {
     const amount = BigInt(rawAmount);
-    return Number(amount) / Math.pow(10, decimals);
+    const val = Number(amount) / Math.pow(10, decimals);
+    if (format) {
+        return formatNumber(val)
+    } else {
+        return val;
+    }
+}
+
+export function formatNumber(num) {
+    let [integerPart, decimalPart] = num.toString().split('.');
+
+    integerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+
+    return decimalPart ? `${integerPart}.${decimalPart}` : integerPart;
 }
